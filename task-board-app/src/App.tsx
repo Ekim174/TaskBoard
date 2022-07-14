2e8710b9345c0497e6a6d53972972bbab2e04794
Macbook <macbook@MacBook-Air-Macbook.local>
2022-07-14 12:27:57 +0500

    sort and random data

--- task-board-app/src/App.tsx
+++ task-board-app/src/App.tsx
@@ -1,12 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import './App.css';
-import { tasksData, borderColumnList } from './constants/tasksData'
+import { generateTaskData, boardColumns, importanceList} from './constants/tasksData'
 import cn from "classnames";
+import styled from './App.module.scss'
 
 
 function App() {
 
-  const [taskList, setTaskList] = useState<any>(tasksData)
+  const [taskList, setTaskList] = useState<any>([])
   const [currentTask, setCurrentTask] = useState(null)
   const [currentColumn, setCurrentColumn] = useState(null)
   const [isDraggable, setIsDraggable] = useState(false)
@@ -35,37 +35,62 @@ function App() {
     setTaskList([...updateTaskList])
   };
 
-  const rowClass = cn('board-row',
+  const rowClass = cn(styled['board-row'],
     {'draggable': isDraggable}
   )
 
+  const sort = (data: any) => {
+    const result = data.sort((a: any, b: any): any => {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (importanceList.indexOf(a.importance) > importanceList.indexOf(b.importance)) {
+        return 1
+      }
+      if (importanceList.indexOf(a.importance) < importanceList.indexOf(b.importance)) {
+        return -1
+      }
+      return 0;
+    })
+    setTaskList(result)
+  }
+  useEffect(() => {
+    !taskList.length ? sort(generateTaskData()) : sort(taskList)
+  }, [taskList])
+
 
   return (
-    <div className='container'>
-      {borderColumnList.map(column =>
-        <div
-          className={rowClass}
-          onDragOver={(e) => dragOverHandler(e)}
-          onDrop={(e) => dropHandler(e, column)}
-          onDragLeave={(e) => dragLeaveHandler(e, column)}
-        >
-          <h2>{column}</h2>
-            {taskList.length > 0 && taskList.map((task: any) =>
-              task.status === column &&
-                <div
-                    onDragOver={(e) => dragOverHandler(e)}
-                    onDragLeave={(e) => dragLeaveHandler(e, column, task)}
-                    onDragStart={(e) => dragStartHandler(e, column, task)}
-                    onDragEnd={(e) => dragEndHandler(e, column, task)}
-                    onDrop={(e) => dropHandler(e, column, task)}
-                    className={cn('task', `${task.importance.toLowerCase()}`)}
-                    draggable={true}>
-                    <span>{task.task_number}</span>
-                    <span>{task.task_name}</span>
-                </div>
-            )}
-        </div>
-      )}
+    <div className={styled.container}>
+      <h1>Task Board</h1>
+      <div className={styled.board}>
+        {boardColumns.map(column =>
+          <div
+            className={rowClass}
+            onDragOver={(e) => dragOverHandler(e)}
+            onDrop={(e) => dropHandler(e, column)}
+            onDragLeave={(e) => dragLeaveHandler(e, column)}
+          >
+            <h2 className={styled['colum-title']}>{column}</h2>
+              {taskList.length && taskList.map((task: any) =>
+                task.status === column &&
+                  <div
+                      onDragOver={(e) => dragOverHandler(e)}
+                      onDragLeave={(e) => dragLeaveHandler(e, column, task)}
+                      onDragStart={(e) => dragStartHandler(e, column, task)}
+                      onDragEnd={(e) => dragEndHandler(e, column, task)}
+                      onDrop={(e) => dropHandler(e, column, task)}
+                      className={cn(styled['task'], styled[`${task.importance.toLowerCase()}`])}
+                      draggable={true}>
+                      <span>{task.task_number}</span>
+                      <span>{task.task_name}</span>
+                  </div>
+              )}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- task-board-app/src/constants/tasksData.ts
+++ task-board-app/src/constants/tasksData.ts
@@ -1,104 +1,32 @@
 const taskNameList = ['Feature', 'Bug Fix', 'Update', 'Research', 'Content'];
-const nameList = ['Вуди Аллен', 'Мэрил Стрип', 'Роджер Дикинс', 'Аль Пачино',];
-export const borderColumnList = ['PLAN', 'IN PROGRESS', 'TESTING','DONE'];
-const importanceList = ['MUST', 'SHOULD', 'COULD'];
+const nameList = [
+  {name: 'Вуди', surname: 'Аллен'},
+  {name: 'Мэрил', surname: 'Стрип'},
+  {name: 'Роджер', surname: 'Дикинс'},
+  {name: 'Аль', surname: 'Пачино'},
+];
+export const boardColumns = ['PLAN', 'IN PROGRESS', 'TESTING','DONE'];
+export const importanceList = ['MUST', 'SHOULD', 'COULD'];
 
-export const tasksData = [
-  {
-    task_number: 'TSK-0001',
-    task_name: 'Feature',
-    name: 'Вуди',
-    surname: 'Аллен',
-    status: 'PLAN',
-    importance: 'MUST',
-    date: '02/13/2013',
-  },
-  {
-    task_number: 'TSK-0002',
-    task_name: 'Bug Fix',
-    name: 'Вуди',
-    surname: 'Аллен',
-    status: 'IN PROGRESS',
-    importance: 'SHOULD',
-    date: '02/13/2013',
-  },
-  {
-    task_number: 'TSK-0003',
-    task_name: 'Update',
-    name: 'Вуди',
-    surname: 'Аллен',
-    status: 'TESTING',
-    importance: 'MUST',
-    date: '02/13/2013',
-  },
-  {
-    task_number: 'TSK-0004',
-    task_name: 'Research',
-    name: 'Вуди',
-    surname: 'Аллен',
-    status: 'DONE',
-    importance: 'COULD',
-    date: '02/13/2013',
-  },
-  {
-    task_number: 'TSK-0001',
-    task_name: 'Feature',
-    name: 'Вуди',
-    surname: 'Аллен',
-    status: 'PLAN',
-    importance: 'MUST',
-    date: '02/13/2013',
-  },
-  {
-    task_number: 'TSK-0002',
-    task_name: 'Bug Fix',
-    name: 'Вуди',
-    surname: 'Аллен',
-    status: 'IN PROGRESS',
-    importance: 'SHOULD',
-    date: '02/13/2013',
-  },
-  {
-    task_number: 'TSK-0003',
-    task_name: 'Update',
-    name: 'Вуди',
-    surname: 'Аллен',
-    status: 'TESTING',
-    importance: 'MUST',
-    date: '02/13/2013',
-  },
-  {
-    task_number: 'TSK-0004',
-    task_name: 'Research',
-    name: 'Вуди',
-    surname: 'Аллен',
-    status: 'DONE',
-    importance: 'COULD',
-    date: '02/13/2013',
-  },
+const getRandomInt = (max: number) => {
+  return Math.floor(Math.random() * max);
+};
 
-
-]
-
-
-// const getRandomInt = (max: number) => {
-//   return Math.floor(Math.random() * max);
-// };
-//
-// export const generateTaskData = () => {
-//   const tasksData = [];
-//   for (let i = 1; i <= 20; i += 1) {
-//     const taskUnit = {
-//       task_number: `TSK-00${ i < 10 ? `0${i}` : i }`,
-//       task_name: taskNameList[getRandomInt(taskNameList.length)],
-//       name: 'Вуди',
-//       surname: 'Аллен',
-//       status: statusList[getRandomInt(statusList.length)],
-//       importance: 'MUST',
-//       date: '02/13/2013',
-//     }
-//     tasksData.push(taskUnit)
-//   }
-//   console.log(tasksData)
-// }
-// generateTaskData()
+export const generateTaskData = () => {
+  const tasksData = [];
+  for (let i = 1; i <= 20; i += 1) {
+    const number = i < 10 ? `0${i}` : i;
+    const nameRandomInt = getRandomInt(nameList.length)
+    const taskUnit = {
+      task_number: `TSK-00${number}`,
+      task_name: taskNameList[getRandomInt(taskNameList.length)],
+      name: nameList[nameRandomInt].name,
+      surname: nameList[nameRandomInt].surname,
+      status: boardColumns[getRandomInt(boardColumns.length)],
+      importance: importanceList[getRandomInt(importanceList.length)],
+      date: '02/13/2013',
+    }
+    tasksData.push(taskUnit);
+  }
+  return tasksData;
+}
