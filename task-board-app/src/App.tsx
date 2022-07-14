c465dc83905a4d4551ec99118a56e398c928f1bd
Macbook <macbook@MacBook-Air-Macbook.local>
2022-07-14 09:42:18 +0500

    feat Drag&Drop

--- task-board-app/src/App.tsx
+++ task-board-app/src/App.tsx
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import { tasksData, borderColumnList } from './constants/tasksData'
+import cn from "classnames";
+
 
 function App() {
+
+  const [taskList, setTaskList] = useState<any>(tasksData)
+  const [currentTask, setCurrentTask] = useState(null)
+  const [currentColumn, setCurrentColumn] = useState(null)
+  const [isDraggable, setIsDraggable] = useState(false)
+
+  const dragOverHandler: any = (e: any)=> {
+    e.preventDefault()
+  };
+
+  const dragLeaveHandler: any = (e: any, column: any, task: any) => {
+
+  };
+
+  const dragStartHandler: any = (e: any, column: any, task: any) => {
+    setCurrentTask(task)
+    setCurrentColumn(column)
+  };
+
+  const dragEndHandler: any = (e: any, column: any, task: any) => {
+  };
+
+  const dropHandler: any = (e: any, column: any, task: any) => {
+    e.preventDefault()
+    const currentIndex = taskList.indexOf(currentTask);
+    const updateTaskList = taskList
+    updateTaskList[currentIndex].status = column
+    setTaskList([...updateTaskList])
+  };
+
+  const rowClass = cn('board-row',
+    {'draggable': isDraggable}
+  )
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='container'>
+      {borderColumnList.map(column =>
+        <div
+          className={rowClass}
+          onDragOver={(e) => dragOverHandler(e)}
+          onDrop={(e) => dropHandler(e, column)}
+          onDragLeave={(e) => dragLeaveHandler(e, column)}
         >
-          Learn React
-        </a>
-      </header>
+          <h2>{column}</h2>
+            {taskList.length > 0 && taskList.map((task: any) =>
+              task.status === column &&
+                <div
+                    onDragOver={(e) => dragOverHandler(e)}
+                    onDragLeave={(e) => dragLeaveHandler(e, column, task)}
+                    onDragStart={(e) => dragStartHandler(e, column, task)}
+                    onDragEnd={(e) => dragEndHandler(e, column, task)}
+                    onDrop={(e) => dropHandler(e, column, task)}
+                    className={cn('task', `${task.importance.toLowerCase()}`)}
+                    draggable={true}>
+                    <span>{task.task_number}</span>
+                    <span>{task.task_name}</span>
+                </div>
+            )}
+        </div>
+      )}
     </div>
   );
 }
